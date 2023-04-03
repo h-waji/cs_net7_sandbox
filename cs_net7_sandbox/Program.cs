28fd07be7650736247ee83a7d63220574471d7a2
wajima <goo.hiro.w@gmail.com>
2023-04-03 20:33:44 +0900

    class

--- cs_net7_sandbox/Program.cs
+++ cs_net7_sandbox/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("VSCodeとUbuntu使いたい");
+﻿Console.WriteLine("使いづらいけど優秀なVisual Studio");
+
+User tom = new User("Tom");
+tom.SayHi();
+
+class User
+{
+    public string name;
+
+    public User() : this("nobody") { }
+
+    public User(string name) 
+    {
+        this.name = name;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi. I'm {name}");
+    }
+}
